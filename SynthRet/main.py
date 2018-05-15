44c5258f105c3033f50ca25ce51c107416ad6d07
jannes <jannes.hp@t-online.de>
2018-05-15 13:09:58 +0200

    add main structure

--- SynthRet/main.py
+++ SynthRet/main.py
@@ -1,4 +1,27 @@
+from utils import merge, addIllumination
 import numpy as np
 
+def main():
+    bkg = generateBackgroundAndFovea()
+    vt = generateVesselsTree()
+    od = generateOpticalDisc()
+    merged = merge(bkg, vt, od)
+    return addIllumination(merged)
+
+# generate an image with the background and fovea
+def generateBackgroundAndFovea():
+    img = [300, 300, 4]
+    return img
+
+# generate an image containing the vessels tree
+def generateVesselsTree():
+    img = [300, 300, 4]
+    return img
+
+# generate an image with the optical disc
+def generateOpticalDisc():
+    img = [300, 300, 4]
+    return img
+
 if __name__ == '__main__':
-    print("HI")
+    main()
---
+++ SynthRet/utils.py
@@ -0,0 +1,19 @@
+# imports
+
+# functions
+
+# 
+def eval():
+    return 0.0
+
+#
+def merge(*args):
+    finalImage = [300, 300, 4]
+    # TODO merge all arguments. first is the lowest layer
+    for i in args:
+        # TODO
+    return finalImage
+
+#
+def addIllumination(img):
+    return img
