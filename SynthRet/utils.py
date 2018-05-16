be2a6edf80c35e8cf977782b254963393e917a39
Moriaty <sj86033503@163.com>
2018-05-16 17:28:41 +0200

    illumination adjustment done

--- SynthRet/utils.py
+++ SynthRet/utils.py
@@ -1,4 +1,5 @@
 # imports
+from PIL import Image, ImageEnhance
 
 # functions
 
@@ -16,6 +17,24 @@ def merge(*args):
     return finalImage
 
 #
-def addIllumination(img):
-    ss
+def addIllumination(image):
+    
+    # set parameters
+    brightness = 2
+    color = 3  
+    contrast = 3  
+    sharpness = 3.0
+
+    # enhance brightness
+    image1 = ImageEnhance.Brightness(image).enhance(brightness)  
+
+    # enhance color
+    image2 = ImageEnhance.Color(image1).enhance(color)   
+    
+    # enhance contrase 
+    image3 = ImageEnhance.Contrast(image2).enhance(contrast)   
+    
+    # enhance sharpness 
+    img = ImageEnhance.Sharpness(image3).enhance(sharpness)  
+
     return img
