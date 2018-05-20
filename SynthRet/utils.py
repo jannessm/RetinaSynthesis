491ebc8ecd0a5a8798c93f48a70774332c794379
Xuelei Chen <365833744@qq.com>
2018-05-20 11:26:00 +0200

    add merge function

--- SynthRet/utils.py
+++ SynthRet/utils.py
@@ -2,19 +2,65 @@
 
 # functions
 
-# 
+#
+import cv2
+import numpy as np
+from skimage import img_as_ubyte
+import skimage.io as io
+
 def eval():
     return 0.0
 
-#
-def merge(*args):
-    finalImage = [300, 300, 4]
-    # TODO merge all arguments. first is the lowest layer
-    for i in args:
-        # TODO
-        continue
-    return finalImage
+#merge 3-chanel RGB images
+def merge3c(collect):
+    #***the size of images***
+    ncol=128
+    nrow=128
+    #initial black image
+    finalimage = np.zeros((ncol, nrow, 3),np.uint8)
+    #merge layers
+    for n in range(len(collect)):
+        img = collect[n]
+        for i in range(ncol):
+            for j in range(nrow):
+                if (img[i,j,:].max() > 0):
+                    finalimage[i,j,:] = img[i,j,:]
+    return finalimage
+
+#merge 4-chanel RGBA images
+def merge4c(collect):
+    #***the size of images***
+    ncol=300
+    nrow=300
+    #initial black image
+    dimg = np.zeros((ncol, nrow, 4),np.uint8)
+    #merge layers
+    for n in range(len(collect)):
+        img = collect[n]
+        for i in range(ncol):
+            for j in range(nrow):
+                #the pixel in new image is not black
+                if (img[i,j,0:3].max() > 0):
+                    #the pixel in old image is black, replace directly
+                    if (dimg[i,j,0:3].max() == 0):
+                        dimg[i,j,:] = img[i,j,:]
+                    #the pixel in old image is not black, alpha blending
+                    else:
+                        al0 = dimg[i,j,3]/255.0
+                        al1 = img[i,j,3]/255.0
+                        dimg[i,j,3] = ((al1+al0*(1-al1))*255).astype(np.uint8)
+                        print dimg[i,j,3]
+                        dimg[i,j,0:3] = (
+                                (img[i,j,0:3]*al1 + dimg[i,j,0:3]*al0*(1-al1))
+                                /al1+al0*(1-al1)
+                                ).astype(np.uint8)
+                        
+    return dimg
 
 #
 def addIllumination(img):
     return img
+
+#collect = io.ImageCollection("./*.png")
+#d=merge4c(collect)
+#io.imshow(d)
