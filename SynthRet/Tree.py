95871224c58a559a6089699f44051acdecf1346c
Moriaty <sj86033503@163.com>
2018-05-23 19:41:03 +0200

    treeMap

--- SynthRet/Tree.py
+++ SynthRet/Tree.py
@@ -26,14 +26,21 @@ class Tree:
             cov = self.coverage()
 
     def createTreeMap(self):
-        #treeMap = np.zeros((300, 300, 4))
+        treeMap = np.zeros((300, 300, 4))
         # draw all branches onto treeMap
-        x,y = np.array(zip(*self.branches))   # seperate x and y coordinates from Branches
+        x,y = np.array(zip(*self.branches))     # seperate x and y coordinates from Branches
         # interpolate 
         s = 0   # smoothing condition (0 means passing all points)
         tck, t = interpolate.splprep([x, y], s=s) 
         xi, yi = interpolate.splev(np.linspace(t[0], t[-1], 200), tck) 
-        treeMap = np.array(zip(np.append(xi,x),np.append(yi,y)))
+        treeCoord = np.array(zip(np.append(xi,x),np.append(yi,y)))      # get all tree points
+        for coord in treeCoord:
+            x,y = int(round(coord[0])),int(round(coord[1]))     # round and int
+            treeMap[x][y]=[255,255,255,1]       # set value
+            ### Print
+        # cv2.circle(bkg,(a,b),1,treeMap[a][b][0:3]*treeMap[a][b][3])
+        # cv2.imshow("show",...)
+        # cv2.waitKey(0)
         return treeMap
 
     def coverage(self):
