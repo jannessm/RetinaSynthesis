ba63b1e0a999998e55c5362cea1a53271aa4c0f4
jannes <jannes.hp@t-online.de>
2018-06-01 16:34:59 +0200

    added time measurement and save option for main func

--- SynthRet/main.py
+++ SynthRet/main.py
@@ -6,22 +6,38 @@ from PIL import Image, ImageDraw
 from utils import mergeLayer, addIllumination, showImage
 import cv2
 from skimage import io, transform, draw, data
-from scipy.misc import imread
+from scipy.misc import imread, imsave
 import math
 import os 
 from OpticalDisc import generateOpticalDisc
 from multiprocessing.dummy import Pool
+import time
 
-def main(i=0):
+'''
+    generate synthetic images. if you want to save the generated files adjust save and path to your needs.
+    path is relative to this file.
+'''
+def generateImages(i=0, save=False, path="/../syntheticImages/"):
     bkg, fovea = generateBackgroundAndFovea()
     od_img, od = generateOpticalDisc()
     vt, groundTruth = generateVesselsTree(fovea, od)
     merged = mergeLayer([bkg, od_img, vt])
     image = addIllumination(merged)
+    image = addMask(image)
+    gt = addMask(groundTruth)
+    if save:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = dir_path + path
+        if not os.path.exists(path + "images"):
+            os.makedirs(path + "images")
+        if not os.path.exists(path + "groundtruth"):
+            os.makedirs(path + "groundtruth")
+        imsave(path + "images/" + str(i+1) + ".png", image)
+        imsave(path + "groundtruth/" + str(i+1) + ".png", gt)
     return addMask(image), addMask(groundTruth)
 
 # generate an image with the background and fovea
-def generateBackgroundAndFovea(): #TODO where is the gradient and the red tissue?
+def generateBackgroundAndFovea():
     img=np.zeros((300, 300, 4),np.uint8)            
     img[:,:,]=[255,127,36,255]
     #macula
@@ -62,11 +78,21 @@ def addMask(image):
     return mergeLayer([image, final_mask])
 
 if __name__ == '__main__':
-    k = 3                               # amount of pictures to generate
-    threads = Pool(k)
-    res = threads.map(main, range(k))
+    k = 10                               # amount of pictures to generate
+
+    if k > 20:                           # limit threads to upper boundary 20
+        nthreads = 20
+    else:
+        nthreads = k
+    
+    start = time.time()                 # start timer
+
+    threads = Pool(nthreads)            # generate k images in parallel
+    res = threads.map(generateImages, range(k))
     threads.close()
     threads.join()
 
-    showImage(list(np.asarray(res)[:,0])) # imgs
-    showImage(list(np.asarray(res)[:,1])) # ground truths
+    print("\n\n" + str(k) + " pictures needed " + str(time.time() - start) + " sec!\n")
+
+    showImage(list(np.asarray(res)[:,0])) # show generated images
+    showImage(list(np.asarray(res)[:,1])) # show ground truths
