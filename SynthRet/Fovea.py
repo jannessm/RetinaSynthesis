20e563a5252daafffe4f4f87ae1a6a8b8ef6bf9a
kexin zhang <kexinzhang1996@hotmail.com>
2018-06-25 18:30:20 +0200

    remove border

--- SynthRet/Fovea.py
+++ SynthRet/Fovea.py
@@ -4,6 +4,7 @@ import random
 import matplotlib.pyplot as plt
 from skimage import io, transform, draw, data
 from perlinNoise import getTexture
+import cv2
 # generate an image with the background and fovea
 def generateBackgroundAndFovea():
        img=np.zeros((300, 300, 4),np.uint8)
@@ -13,17 +14,18 @@ def generateBackgroundAndFovea():
        img1=getTexture()
        img[:,:,]=img1[:,:,]*255
        PosFovea=(cy,cx)
-       rr,cc=draw.circle(cy,cx,25.4)
-       gbx=cx + np.random.randint(-2,3)
-       gby=cy + np.random.randint(-2,3)
+       rr,cc=draw.circle(cy,cx,25.2)
+       gbx=cx + np.random.randint(-1,1)
+       gby=cy + np.random.randint(-1,1)
        for i in range(len(rr)):
                 y=rr[i]
                 x=cc[i]
                 img[y,x,0] = rValue(x,y,cx,cy) 
                 img[y,x,1] = gValue(x,y,cx,cy,gbx,gby) 
                 img[y,x,2] = bValue(x,y,cx,cy,gbx,gby)
-
-
+       kernel_size = (5, 5);
+       sigma = 1;
+       img = cv2.GaussianBlur(img, kernel_size, sigma);
        return img,PosFovea
 
 	
@@ -36,9 +38,9 @@ def rValue(x,y,cx,cy):
     R=np.sqrt(cx**2+cy**2)
     red= 198*math.exp(-0.3*d/(R*2))
     """
-    r=178  
-    a = 0.05
-    ther = 10  
+    r=189 
+    a = 0.036
+    ther = 8
     exponentr = -((x-cx)/ther)**2 - ((y-cy)/ther)**2
     #red =  zr - 1/(a+math.exp(exponentr))
     red = r+1/(a+math.exp(exponentr))
@@ -53,15 +55,15 @@ def bValue(x,y,cx,cy,gbx,gby):
     blue= 28*math.exp(-0.6*d/(R*2))
     """
     #r parameters
-    zr =28
-    a = 0.05
+    zr =36
+    a = 0.1
     ther =30
     exponentr = -((x-cx)/ther)**2 - ((y-cy)/ther)**2
     r =  zr+1/(a+math.exp(exponentr))
     
     #parameters
-    k = 6
-    thegb =10
+    k = 10
+    thegb =40
     #calculate bchanel values
     exponentgb = -((x-gbx)/thegb)**2 - ((y-gby)/thegb)**2
     #blue = r+k*math.exp(exponentgb)
@@ -78,19 +80,28 @@ def gValue(x,y,cx,cy,gbx,gby):
     green= 50*math.exp(-0.1*d/(R*2))
     """
     #r parameters
-    zr = 50
-    a = 0.04
+    zr = 58
+    a = 0.1
     ther = 30
     exponentr = -((x-cx)/ther)**2 - ((y-cy)/ther)**2
     r =  zr+1/(a+math.exp(exponentr))   
     #parameters
-    k =12
-    thegb =10
+    k =10
+    thegb =40
     #calculate gchanel values
     exponentgb = -((x-gbx)/thegb)**2 - ((y-gby)/thegb)**2
     #green = r+k*math.exp(exponentgb)
     green = r-k*math.exp(exponentgb)
     
     return green
+# print(rValue(0,0,0,0))
+# print(rValue(12,12,0,0))
+# print(bValue(0,0,0,0,0,0))
+# print(bValue(12,12,0,0,0,0))
+# print(gValue(0,0,0,0,0,0))
+# print(gValue(12,12,0,0,0,0))
 
-#io.imshow(d)
+
+# b,p=generateBackgroundAndFovea()
+# plt.imshow(b)
+# plt.savefig("E:/3.png")
