ad1350642f8cc13c262a2508f071c170f1359f00
Kexin Zhang <kexin.zhang@campus.tu-berlin.de>
2018-07-24 16:51:49 +0200

    Update Fovea.py

--- SynthRet/Background/Fovea.py
+++ SynthRet/Background/Fovea.py
@@ -4,66 +4,77 @@ import random
 import matplotlib.pyplot as plt
 from skimage import io, transform, draw, data
 from perlinNoise import getTexture
+
 # generate an image with the background and fovea
 def generateBackgroundAndFovea():
-       img=np.zeros((300, 300, 4),np.uint8)     
+    
+       img=np.zeros((300, 300, 4),np.uint8)  
+       #fovea position
        cx=150+np.random.randint(-10,10)
        cy=150+np.random.randint(-10,10)
+       PosFovea=(cy,cx)
+       #Perlin noise texture
        img1=getTexture()
        img[:,:,]=img1[:,:,]*255
-       PosFovea=(cy,cx)
+      
        rr,cc=draw.circle(cy,cx,25.2)
        gbx=cx + np.random.randint(-1,1)
        gby=cy + np.random.randint(-1,1)
        for i in range(len(rr)):
                 y=rr[i]
                 x=cc[i]
+                #r,g,b channel intensity value
                 img[y,x,0] = rValue(x,y,cx,cy) 
                 img[y,x,1] = gValue(x,y,cx,cy,gbx,gby) 
                 img[y,x,2] = bValue(x,y,cx,cy,gbx,gby)
        
        return img,PosFovea
 
-	
+#RGB intensity model based on equations in the paper of Samuele Fiorini12 et al 	
 def rValue(x,y,cx,cy):
-
+    #amplitude
     r=189.00 
-    a = 0.036
+    a = 0.0364
+    #the spread of the distribution surface
     ther = 8.10
-    exponentr = -((x-cx)/ther)**2 - ((y-cy)/ther)**2
-    red = r+1/(a+math.exp(exponentr))
-    
-    return red	
-def bValue(x,y,cx,cy,gbx,gby):
-    
-
-    zr =36.00
+    # oscillating
+    h=1.22
+    w=2.0387
+    exponentr = -((x-cx+h*np.cos(w))/ther)**2 - ((y-cy+h*np.cos(w))/ther)**2
+    #r intensity value
+    red = r+1/(a+math.exp(exponentr))  
+    return red
+	
+def bValue(x,y,cx,cy,gbx,gby): 
+    #amplitude
+    zr =36.0
     a = 0.1
+    #control the spread
     ther =30.20
     exponentr = -((x-cx)/ther)**2 - ((y-cy)/ther)**2
     r =  zr+1/(a+math.exp(exponentr))
+    #amplitude of the inner part
     k = 10.10
     thegb =40.10
     exponentgb = -((x-gbx)/thegb)**2 - ((y-gby)/thegb)**2
-    blue = r-k*math.exp(exponentgb)
-    
+    #b intensity value
+    blue = r-k*math.exp(exponentgb)   
     return blue
 
 def gValue(x,y,cx,cy,gbx,gby):
-
+    #amplitude
     zr = 58.00
-    a = 0.1
+    a = 0.1029
+    #control the spread
     ther = 30.10
     exponentr = -((x-cx)/ther)**2 - ((y-cy)/ther)**2
-    r =  zr+1/(a+math.exp(exponentr))   
+    r =  zr+1/(a+math.exp(exponentr))
+    #amplitude of the inner part
     k =10.10
     thegb =40.10
     exponentgb = -((x-gbx)/thegb)**2 - ((y-gby)/thegb)**2
-    green = r-k*math.exp(exponentgb)
-    
+    #g value
+    green = r-k*math.exp(exponentgb)  
     return green
 
-#b,p=generateBackgroundAndFovea()
-#plt.figure()
-#plt.imshow(b)
-##plt.savefig("E:/a.png")
+
