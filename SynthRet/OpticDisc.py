3dda87dd0eb8f185733293870dd243cf08f3881a
Xuelei Chen <365833744@qq.com>
2018-10-04 20:01:14 +0800

    add the size parameter in OD

--- SynthRet/OpticDisc.py
+++ SynthRet/OpticDisc.py
@@ -2,8 +2,8 @@ import numpy as np
 from skimage import io
 import time
 
-def generateOpticDisc(fovea):
-    odimg = np.zeros((300, 300, 4),np.uint8)
+def generateOpticDisc(fovea,size):
+    odimg = np.zeros((size, size, 4),np.uint8)
     
     
     rx = fovea[0] + 77*np.random.choice([1]) + np.random.randint(-5,6)
@@ -11,15 +11,15 @@ def generateOpticDisc(fovea):
     gbx = rx + np.random.randint(-2,3)
     gby = ry + np.random.randint(-2,3)
     
-    for i in np.arange(300):
-        for j in np.arange(300):
+    for i in np.arange(size):
+        for j in np.arange(size):
             if odr(i,j,rx,ry) > 245:
                 odimg[j,i,0] = odr(i,j,rx,ry)
                 odimg[j,i,1] = odg(i,j,rx,ry,gbx,gby) 
                 odimg[j,i,2] = odb(i,j,rx,ry,gbx,gby)
                 odimg[j,i,3] = 255
 
-    return np.transpose(odimg,(1,0,2)), [rx,ry] #TODO select random point according to fovea pos.
+    return np.transpose(odimg,(1,0,2)), [rx,ry] 
 
 def odr(x,y,rx,ry):
     #parameters
@@ -30,7 +30,7 @@ def odr(x,y,rx,ry):
     yr = ry+np.cos(w*time.time())
     
     a = 0.0207176
-    sr = 11.9622
+    sr = 11.9622*size/300
     
     #calculate rchanel values
     exponentr = -((x-xr)/sr)**2 - ((y-yr)/sr)**2
@@ -44,7 +44,7 @@ def odb(x,y,rx,ry,bx,by):
     xr = rx
     yr = ry
     a = 0.0461424
-    sr = 5.79272
+    sr = 5.79272*size/300
 
     exponentr = -((x-xr)/sr)**2 - ((y-yr)/sr)**2
     r =  zr - 1/(a+np.exp(exponentr))
@@ -53,7 +53,7 @@ def odb(x,y,rx,ry,bx,by):
     kb = 2.08531+(np.random.random_sample()-0.5)*0.01
     xb = bx
     yb = by
-    sb = 3.90212
+    sb = 3.90212*size/300
     
     #calculate bchanel values
     exponentgb = -((x-xb)/sb)**2 - ((y-yb)/sb)**2
@@ -66,16 +66,16 @@ def odg(x,y,rx,ry,gx,gy):
     xr = rx
     yr = ry
     a = 0.0403873
-    ther = 13.3931
+    sr = 13.3931*size/300
 
-    exponentr = -((x-xr)/ther)**2 - ((y-yr)/ther)**2
+    exponentr = -((x-xr)/sr)**2 - ((y-yr)/sr)**2
     r =  zr - 1/(a+np.exp(exponentr))
     
     #parameters
     kg = 63.1894+(np.random.random_sample()-0.5)*1
     xg = gx
     yg = gy
-    sg = 8.05019
+    sg = 8.05019*size/300
     
     #calculate gchanel values
     exponentg = -((x-xg)/sg)**2 - ((y-yg)/sg)**2
@@ -83,5 +83,5 @@ def odg(x,y,rx,ry,gx,gy):
     return green
 
 ##OD generate test
-#d,p=generateOpticDisc([150,150])
+#d,p=generateOpticDisc([150,150],300)
 #io.imshow(d)
